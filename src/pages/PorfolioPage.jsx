93ef6e865b457a4db540d2cef574e28df2cd0c2a
Jandrei Timoteo <jandrei_tim@yahoo.com>
2024-01-21 14:44:04 -0800

    refactored folders

---
+++ src/pages/PorfolioPage.jsx
@@ -0,0 +1,12 @@
+import image from '../assets/images/placeholder.png'
+
+export default function PortfolioPage() {
+    return (
+      <div>
+        <h1>
+          Portfolio page
+        </h1>
+        <img src={image}></img>
+      </div>
+    )
+  }
