f1d759ba04c67cd15e0d8185949022f519fe7997
Jandrei Timoteo <jandrei_tim@yahoo.com>
2024-01-23 15:35:42 -0800

    Added Porfoliopage.jsx codes

--- src/pages/PorfolioPage.jsx
+++ src/pages/PorfolioPage.jsx
@@ -1,12 +1,21 @@
-import image from '../assets/images/placeholder.png'
+import { Row, Col } from 'react-bootstrap';
+import Project from '../components/Project';
+import data from '../assets/data/projectsData';
 
 export default function PortfolioPage() {
-    return (
-      <div>
-        <h1>
-          Portfolio page
-        </h1>
-        <img src={image}></img>
-      </div>
-    )
-  }
+  return (
+    <Row >
+      <h2 className="my-3">Portfolio</h2>
+      {/* Map over projects data and display the Project component for each project */}
+      {data.map((project) => (
+        <Col key={project.id} sm={12} md={6} lg={4} xl={3}>
+          <Project
+            title={project.title}
+            image={project.image}
+            github={project.github}
+          />
+        </Col>
+      ))}
+    </Row>
+  );
+}
